13718f9c55f08137202de6c3276ff0ca444c6d5a
Alexander Castro <alexander.castro01@uptc.edu.co>
2021-04-15 11:44:09 -0500

    Add test

---
+++ test/calculator_chai_assert.js
@@ -0,0 +1,9 @@
+var assert    = require("chai").assert;
+
+describe("Calcultator tests using ASSERT interface from CHAI module: ", function() {
+ describe("Check addTested Function: ", function() {
+ it("Check the returned value using: assert.equal(value,'value'): ", function() {
+ assert.equal(2, 2);
+ });
+ }); 
+});
